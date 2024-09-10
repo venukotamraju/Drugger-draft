af11e3632c78fd45ff6ea3b3eac35c0eedf2604e
venukotamraju <kotamraju.venugopal@gmail.com>
2024-09-10 18:38:29 +0530

    added PUT and DELETE for customer details, moved everything into server dir

--- server/customers/views.py
+++ server/customers/views.py
@@ -28,6 +28,38 @@ class CustomerListView(APIView):
             return Response(details_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         return Response(customer_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request):
+        print(request.data)
+        try:
+            customer = CustomerList.objects.get(customer_id = request.data["customer_id"])
+            serializer = CustomerListSerializer(customer, data = request.data)
+            # FOR CUSTOMER NAME
+            if serializer.is_valid():
+                serializer.save()
+            # FOR CUSTOMER DETAILS
+                if request.data['details']:
+                    details = request.data["details"]
+                    details.update({"customer_id":serializer.data["customer_id"]})
+                    details_instance = CustomerDetails.objects.get(customer_id = customer)
+                    details_serializer = CustomerDetailsSerializer(details_instance, data = details)
+                    if details_serializer.is_valid():
+                        details_serializer.save()
+                        return Response({"name":serializer.data,'details':details.serializer.data},status = status.HTTP_200_OK)
+                    return Response({'name':serializer.data,'details':data_serializer.errors},status = HTTP_206_PARTIAL_CONTENT)
+                return Response(serializer.data,status = HTTP_202_ACCEPTED)
+            return Response(serializer.errors,status = HTTP_400_BAD_REQUEST)
+        except CustomerList.DoesNotExist:
+            return Response({"message":"Customer Does Not Exist"}, status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request):
+        try:
+            customer = CustomerList.objects.get(customer_id = request.data['customer_id'])
+            details = CustomerDetails.objects.filter(customer_id = customer)
+            details.delete()
+            return Response({'message':'Customer Details Deleted Successfully'}, status = status.HTTP_204_NO_CONTENT)
+        except CustomerList.DoesNotExist:
+            return Response({'message':'Customer Does Not Exist'},status = status.HTTP_400_BAD_REQUEST)
+
 class CustomerDetailView(APIView):
     def get(self,request,pk):
         try:
